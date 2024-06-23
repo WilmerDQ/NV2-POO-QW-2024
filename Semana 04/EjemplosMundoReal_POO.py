aaaac74dd74452f278ee73050d2fe35c3a163270
Wilmer Danilo Quitio Piltaxi <wd.quitiop@uea.edu.ec>
2024-06-23 09:45:30 -0500

    Semana 4

---
+++ Semana 04/EjemplosMundoReal_POO.py
@@ -0,0 +1,44 @@
+class Libro:
+    def __init__(self, titulo, autor, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.disponible = True
+
+    def prestar(self):
+        if self.disponible:
+            self.disponible = False
+            return True
+        return False
+
+    def devolver(self):
+        self.disponible = True
+
+class Usuario:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.libros_prestados = []
+
+    def tomar_prestado(self, libro):
+        if libro.prestar():
+            self.libros_prestados.append(libro)
+            return True
+        return False
+
+    def devolver_libro(self, libro):
+        if libro in self.libros_prestados:
+            libro.devolver()
+            self.libros_prestados.remove(libro)
+            return True
+        return False
+
+class Biblioteca:
+    def __init__(self):
+        self.libros = []
+        self.usuarios = []
+
+    def agregar_libro(self, libro):
+        self.libros.append(libro)
+
+    def registrar_usuario(self, usuario):
+        self.usuarios.append(usuario)
